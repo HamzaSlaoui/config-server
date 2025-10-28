1d4d34e68a981a99da21d09c1fa593915568d746
Hamza Slaoui <slaouihza2@gmail.com>
2025-10-28 20:06:41 +0100

    Initial commit

---
+++ src/test/java/com/emsi/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
